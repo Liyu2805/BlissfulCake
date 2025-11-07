253ec90313250403cc8181646b4f062a9ef9eb93
Liyutsa Zirange <Liyutsa@gmail.com>
2025-11-07 08:12:13 +0000

    added a song

--- script.js
+++ script.js
@@ -10,21 +10,3 @@ playButton.addEventListener("click", () => {
   setTimeout(() => {
     playButton.style.transform = "scale(1.2) rotate(-10deg)";
   }, 300);
-
-window.addEventListener('DOMContentLoaded', () => {
-  const song = document.getElementById('song');
-  setTimeout(() => {
-    song.play().catch(err => {
-      console.log("🎵 Autoplay was blocked, waiting for user interaction.");
-    });
-  }, 100);
-});
-
-document.addEventListener("click", () => {
-  const song = document.getElementById("song");
-  if (song) {
-    song.play().catch((e) => {
-      console.log("Autoplay prevented:", e);
-    });
-  }
-});
