3a6d9f76d4f86935fa0c007c0226dfd5f16fb415
Liyutsa Zirange <Liyutsa@gmail.com>
2025-11-10 08:17:27 +0000

    fixes playbutton

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-const playButton = document.getElementById("playButton");
+document.getElementById('mybutton').onclick = function() {
+    window.location.href = 'birthday.html';
+};
 
-playButton.addEventListener("click", () => {
-
-document.body.innerHTML = `
-  <div style="
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: #000;
-  ">
-    <img src="assets/birthday.gif"
-         style="
-           max-width: 90vw;
-           max-height: 90vh;
-           width: auto;
-           height: auto;
-           object-fit: contain;
-         ">
-</div>
-
-  <audio id="song" autoplay loop>
-    <source src="assets/HB.mp3" type="audio/mpeg">
-  </audio>
-`;
-
-  const song = document.getElementById("song");
+const song = document.getElementById("song");
 
   // Try autoplay
-  song.play().catch(() => {
+song.play().catch(() => {
     console.log("Autoplay blocked — waiting for click.");
     document.addEventListener("click", () => song.play(), { once: true });
-  });
 });
